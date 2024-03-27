ec499f838eabc0756a12bb5879aee3dc6b7d2247
sylvain.lafaye <sylvain-lafaye@hotmail.fr>
2024-03-27 12:24:20 +0100

    add props

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,19 @@ import MyTitle from './components/MyTitle';
 import PokemonCard from './components/PokemonCard';
 import './App.css'
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"},
+  {
+    name: "mew",
+  },
+];
+
 function App() {
   return (
     <>
-      <PokemonCard/>
+      <PokemonCard pokemon = {pokemonList[0]}/>
     </>
   )
 }
